d840c228de5a3ec8deb888bef57e9d7b7f21a5e6
Birkin James Diana <birkin.diana@gmail.com>
2022-08-15 18:59:35 -0400

    adds error_check url.

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,12 @@ from x_app import views
 
 
 urlpatterns = [
+    ## main ---------------------------------------------------------
     path( 'info/', views.info, name='info_url' ),
-    path( 'version/', views.version, name='version_url' ),
-    #
+    ## other --------------------------------------------------------
     path( '', views.root, name='root_url' ),
     path( 'admin/', admin.site.urls ),
+    path( 'error_check/', views.error_check, name='error_check_url' ),
+    path( 'version/', views.version, name='version_url' ),
 ]
 
