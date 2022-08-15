31d39a4dcb2a316c3f913202bc754ed626c292ae
Birkin James Diana <birkin.diana@gmail.com>
2022-08-15 18:37:43 -0400

    adds redirect.

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,8 @@ from x_app import views
 urlpatterns = [
     path( 'info/', views.info, name='info_url' ),
     path( 'version/', views.version, name='version_url' ),
-
+    #
+    path( '', views.root, name='root_url' ),
     path( 'admin/', admin.site.urls ),
 ]
 
--- x_app/views.py
+++ x_app/views.py
@@ -1,16 +1,15 @@
 import datetime, json, logging
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from x_app.lib import version_helper
 
 log = logging.getLogger(__name__)
 
 
 def info(request):
-    return HttpResponse("Hello, world. You're at the info-I page.")
-    # resp = render( request, 'info.html', {'foo': 'CC'} )
-    return resp
+    return HttpResponse( "Hello, world." )
 
 
 def version( request ):
@@ -23,3 +22,7 @@ def version( request ):
     output = json.dumps( context, sort_keys=True, indent=2 )
     log.debug( f'output, ``{output}``' )
     return HttpResponse( output, content_type='application/json; charset=utf-8' )
+
+
+def root( request ):
+    return HttpResponseRedirect( reverse('info_url') )
