42dae5231d8e98e42d7bd8b81dcefc6411776d88
Birkin James Diana <birkin.diana@gmail.com>
2022-08-15 19:00:40 -0400

    implements error-check.

--- x_app/views.py
+++ x_app/views.py
@@ -1,5 +1,6 @@
 import datetime, json, logging
 
+from django.conf import settings as project_settings
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -8,8 +9,33 @@ from x_app.lib import version_helper
 log = logging.getLogger(__name__)
 
 
+# -------------------------------------------------------------------
+# main urls
+# -------------------------------------------------------------------
+
+
 def info(request):
     return HttpResponse( "Hello, world." )
+
+
+# -------------------------------------------------------------------
+# support urls
+# -------------------------------------------------------------------
+
+
+def error_check( request ):
+    """ For an easy way to check that admins receive error-emails (in development)...
+        To view error-emails in runserver-development:
+        - run, in another terminal window: `python -m smtpd -n -c DebuggingServer localhost:1026`,
+        - (or substitue your own settings for localhost:1026)
+    """
+    log.debug( f'project_settings.DEBUG, ``{project_settings.DEBUG}``' )
+    if project_settings.DEBUG == True:
+        log.debug( 'triggering exception' )
+        raise Exception( 'Raising intentional exception.' )
+    else:
+        log.debug( 'returing 404' )
+        return HttpResponseNotFound( '<div>404 / Not Found</div>' )
 
 
 def version( request ):
